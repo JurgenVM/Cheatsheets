170581121b5ffe278b11964e9717a45b5d637822
JurgenVM <van.meerhaeghe.jurgen@gmail.com>
2014-10-26 15:26:36 +0100

    init

---
+++ ExportToPdf.ps1
@@ -0,0 +1,5 @@
+﻿foreach($file in (ls .\cheatsheets *.md))
+{
+$name = $file.BaseName
+pandoc .\cheatsheets\$name.md -o .\cheatsheets\pdf\$name.pdf -V geometry:"top=1cm, bottom=1.5cm, left=1cm, right=1cm"
+}
