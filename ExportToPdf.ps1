94291370a8efd30687c368349d66d0274768f570
JurgenVM <van.meerhaeghe.jurgen@gmail.com>
2014-11-28 08:34:29 +0100

    dirty fix

--- ExportToPdf.ps1
+++ ExportToPdf.ps1
@@ -3,3 +3,9 @@
 $name = $file.BaseName
 pandoc .\cheatsheets\$name.md -o .\cheatsheets\pdf\$name.pdf -V geometry:"top=1cm, bottom=1.5cm, left=1cm, right=1cm"
 }
+
+foreach($file in (ls .\checklist *.md))
+{
+$name = $file.BaseName
+pandoc .\checklist\$name.md -o .\checklist\pdf\$name.pdf -V geometry:"top=1cm, bottom=1.5cm, left=1cm, right=1cm"
+}
